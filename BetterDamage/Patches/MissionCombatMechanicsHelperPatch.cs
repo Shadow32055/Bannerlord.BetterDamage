aa26d8ea5e09c0ce12c2ebf1c18a75c76c910763
Shadow32055 <shadow32055@gmail.com>
2024-01-09 23:57:49 -0500

    Initial load

---
+++ BetterDamage/Patches/MissionCombatMechanicsHelperPatch.cs
@@ -0,0 +1,112 @@
+﻿using BetterCore.Utils;
+using HarmonyLib;
+using System;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace BetterDamage.Patches {
+    [HarmonyPatch(typeof(MissionCombatMechanicsHelper))]
+    class MissionCombatMechanicsHelperPatch {
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(MissionCombatMechanicsHelper), "ComputeBlowDamage")]
+        public static void ComputeBlowDamage(ref AttackInformation attackInformation, ref AttackCollisionData attackCollisionData, WeaponComponentData attackerWeapon, DamageTypes damageType, float magnitude, int speedBonus, bool cancelDamage, ref int inflictedDamage, ref int absorbedByArmor) {
+            try {
+                if (attackInformation.AttackerAgentCharacter == null)
+                    return;
+
+                if (attackerWeapon == null)
+                    return;
+
+                switch (attackerWeapon.WeaponClass) {
+                    case WeaponClass.Undefined:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.UndefinedAdjustment);
+                        break;
+                    case WeaponClass.Dagger:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.DaggerAdjustment);
+                        break;
+                    case WeaponClass.OneHandedSword:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.OneHandedSwordAdjustment);
+                        break;
+                    case WeaponClass.TwoHandedSword:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.TwoHandedSwordAdjustment);
+                        break;
+                    case WeaponClass.OneHandedAxe:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.OneHandedAxeAdjustment);
+                        break;
+                    case WeaponClass.TwoHandedAxe:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.TwoHandedAxeAdjustment);
+                        break;
+                    case WeaponClass.Mace:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.MaceAdjustment);
+                        break;
+                    case WeaponClass.Pick:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.PickAdjustment);
+                        break;
+                    case WeaponClass.TwoHandedMace:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.TwoHandedMaceAdjustment);
+                        break;
+                    case WeaponClass.OneHandedPolearm:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.OneHandedPolearmAdjustment);
+                        break;
+                    case WeaponClass.TwoHandedPolearm:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.TwoHandedPolearmAdjustment);
+                        break;
+                    case WeaponClass.LowGripPolearm:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.LowGripPolearmAdjustment);
+                        break;
+                    case WeaponClass.Arrow:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.ArrowAdjustment);
+                        break;
+                    case WeaponClass.Bolt:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.BoltAdjustment);
+                        break;
+                    case WeaponClass.Cartridge:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.CartridgeAdjustment);
+                        break;
+                    case WeaponClass.Bow:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.BowAdjustment);
+                        break;
+                    case WeaponClass.Crossbow:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.CrossbowAdjustment);
+                        break;
+                    case WeaponClass.Stone:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.StoneAdjustment);
+                        break;
+                    case WeaponClass.Boulder:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.BoulderAdjustment);
+                        break;
+                    case WeaponClass.ThrowingAxe:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.ThrowingAxeAdjustment);
+                        break;
+                    case WeaponClass.ThrowingKnife:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.ThrowingKnifeAdjustment);
+                        break;
+                    case WeaponClass.Javelin:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.JavelinAdjustment);
+                        break;
+                    case WeaponClass.Pistol:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.PistolAdjustment);
+                        break;
+                    case WeaponClass.Musket:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.MusketAdjustment);
+                        break;
+                    case WeaponClass.SmallShield:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.SmallShieldAdjustment);
+                        break;
+                    case WeaponClass.LargeShield:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.LargeShieldAdjustment);
+                        break;
+                    case WeaponClass.Banner:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.BannerAdjustment);
+                        break;
+                    case WeaponClass.NumClasses:
+                        inflictedDamage = (int)(inflictedDamage * BetterDamage.Settings.NumClassesAdjustment);
+                        break;
+                }
+            } catch (Exception e) {
+                NotifyHelper.ReportError(BetterDamage.ModName, "MissionCombatMechanicsHelper.ComputeBlowDamage threw exception: " + e);
+            }
+        }
+    }
+}
---
+++ BetterDamage/Settings/MCMSettings.cs
@@ -0,0 +1,130 @@
+﻿using MCM.Abstractions.Attributes;
+using MCM.Abstractions.Attributes.v2;
+using MCM.Abstractions.Base.Global;
+
+namespace BetterDamage.Settings {
+
+    public class MCMSettings : AttributeGlobalSettings<MCMSettings> {
+
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Undefined", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float UndefinedAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Dagger", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float DaggerAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}One Handed Sword", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float OneHandedSwordAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Two Handed Sword", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float TwoHandedSwordAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}One Handed Axe", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float OneHandedAxeAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Two Handed Axe", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float TwoHandedAxeAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Mace", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float MaceAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Pick", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float PickAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Two Handed Mace", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float TwoHandedMaceAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}One Handed Polearm", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float OneHandedPolearmAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Two Handed Polearm", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float TwoHandedPolearmAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Low Grip Polearm", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float LowGripPolearmAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Arrow", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float ArrowAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Bolt", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float BoltAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Cartridge", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float CartridgeAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Bow", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float BowAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Crossbow", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float CrossbowAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Stone", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float StoneAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Boulder", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float BoulderAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Throwing Axe", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float ThrowingAxeAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Throwing Knife", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float ThrowingKnifeAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Javelin", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float JavelinAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Pistol", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float PistolAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Musket", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float MusketAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Small Shield", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float SmallShieldAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Large Shield", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float LargeShieldAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Banner", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float BannerAdjustment { get; set; } = 1f;
+
+        [SettingPropertyGroup("Damage Adjustments")]
+        [SettingPropertyFloatingInteger("{=BC_5th0a2}Num Classes", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        public float NumClassesAdjustment { get; set; } = 1f;
+
+
+
+        public override string Id { get { return base.GetType().Assembly.GetName().Name; } }
+        public override string DisplayName { get { return base.GetType().Assembly.GetName().Name; } }
+        public override string FolderName { get { return base.GetType().Assembly.GetName().Name; } }
+        public override string FormatType { get; } = "xml";
+        public bool LoadMCMConfigFile { get; set; } = true;
+    }
+}
