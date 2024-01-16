5764e356ee1185b05eb2815792d39def985eff09
Shadow32055 <shadow32055@gmail.com>
2024-01-16 14:00:36 -0500

    Fix localization

---
+++ BetterDamage/Localizations/RefValues.cs
@@ -0,0 +1,42 @@
+﻿using TaleWorlds.CampaignSystem.Conversation.Tags;
+
+namespace BetterDamage.Localizations {
+    public class RefValues {
+        public const string DamageAdjText = "{=BDA_QIayEb}Damage Adjustments";
+        public const string MiscText = "{=BDA_dbzXZ2}Misc";
+
+        public const string WepHelperText = "{=BDA_dLalYq}Weapon class helper";
+        public const string WepHelperHint = "{=BDA_mCLHmm}Enable this to display the weapon class the player is currently using on attack, mainly for debug purposes.";
+
+        public const string GenericHint = "{=BC_mAsvxA}Weapon inflicted damage multipler on selected weapon class";
+
+        public const string UndefinedText = "{=BC_ZSbIsF}Undefined";
+        public const string DaggerText = "{=BC_TjomQ2}Dagger";
+        public const string OneHandedSwordText = "{=BC_ee7II0}One Handed Sword";
+        public const string TwoHandedSwordText = "{=BC_WEkoUK}Two Handed Sword";
+        public const string OneHandedAxeText = "{=BC_z1Nbay}One Handed Axe";
+        public const string TwoHandedAxeText = "{=BC_hzdUIg}Two Handed Axe";
+        public const string MaceText = "{=BC_FJMWlB}Mace";
+        public const string PickText = "{=BC_lsTeAp}Pick";
+        public const string TwoHandedMaceText = "{=BC_D5n5aD}Two Handed Mace";
+        public const string OneHandedPolearmText = "{=BC_FCoBWS}One Handed Polearm";
+        public const string TwoHandedPolearmText = "{=BC_u09dy2}Two Handed Polearm";
+        public const string LowGripPolearmText = "{=BC_bkJ0r2}Low Grip Polearm";
+        public const string ArrowText = "{=BC_YI8Yih}Arrow";
+        public const string BoltText = "{=BC_rVS5SJ}Bolt";
+        public const string CartridgeText = "{=BC_czWY0k}Cartridge";
+        public const string BowText = "{=BC_A1imGy}Bow";
+        public const string CrossboxText = "{=BC_72RhMO}Crossbow";
+        public const string StoneText = "{=BC_ajpCa6}Stone";
+        public const string BoulderText = "{=BC_gLBdwZ}Boulder";
+        public const string ThrowingAxeText = "{=BC_4zEEFr}Throwing Axe";
+        public const string ThrowingKnifeText = "{=BC_R12KZK}Throwing Knife";
+        public const string JavelinText = "{=BC_0Uj0zH}Javelin";
+        public const string PistolText = "{=BC_q0GSDf}Pistol";
+        public const string MusketText = "{=BC_CFG9LH}Musket";
+        public const string SmallShiledText = "{=BC_1QmcoL}Small Shield";
+        public const string LargeShieldText = "{=BC_8EOPN0}Large Shield";
+        public const string BannerText = "{=BC_sK0l3C}Banner";
+        public const string NumClassText = "{=BC_EBD34f}Num Classes";
+    }
+}
--- BetterDamage/Settings/MCMSettings.cs
+++ BetterDamage/Settings/MCMSettings.cs
@@ -1,4 +1,5 @@
-﻿using MCM.Abstractions.Attributes;
+﻿using BetterDamage.Localizations;
+using MCM.Abstractions.Attributes;
 using MCM.Abstractions.Attributes.v2;
 using MCM.Abstractions.Base.Global;
 
@@ -7,120 +8,120 @@ namespace BetterDamage.Settings {
     public class MCMSettings : AttributeGlobalSettings<MCMSettings> {
 
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Undefined", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.UndefinedText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float UndefinedAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Dagger", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.DaggerText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float DaggerAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}One Handed Sword", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.OneHandedSwordText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float OneHandedSwordAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Two Handed Sword", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.TwoHandedSwordText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float TwoHandedSwordAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}One Handed Axe", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.OneHandedAxeText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float OneHandedAxeAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Two Handed Axe", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.TwoHandedAxeText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float TwoHandedAxeAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Mace", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.MaceText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float MaceAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Pick", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.PickText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float PickAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Two Handed Mace", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.TwoHandedMaceText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float TwoHandedMaceAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}One Handed Polearm", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.OneHandedPolearmText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float OneHandedPolearmAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Two Handed Polearm", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.TwoHandedPolearmText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float TwoHandedPolearmAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Low Grip Polearm", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.LowGripPolearmText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float LowGripPolearmAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Arrow", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.ArrowText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float ArrowAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Bolt", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.BoltText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float BoltAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Cartridge", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.CartridgeText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float CartridgeAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Bow", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.BowText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float BowAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Crossbow", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.CrossboxText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float CrossbowAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Stone", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.StoneText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float StoneAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Boulder", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.BoulderText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float BoulderAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Throwing Axe", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.ThrowingAxeText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float ThrowingAxeAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Throwing Knife", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.ThrowingKnifeText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float ThrowingKnifeAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Javelin", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.JavelinText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float JavelinAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Pistol", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.PistolText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float PistolAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Musket", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.MusketText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float MusketAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Small Shield", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.SmallShiledText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float SmallShieldAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Large Shield", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.LargeShieldText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float LargeShieldAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Banner", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.BannerText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float BannerAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Damage Adjustments")]
-        [SettingPropertyFloatingInteger("{=BC_5th0a2}Num Classes", 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = "{=BC_C7twl6}Adjust weapon damage")]
+        [SettingPropertyGroup(RefValues.DamageAdjText)]
+        [SettingPropertyFloatingInteger(RefValues.NumClassText, 0f, 5f, "0.0 %", Order = 0, RequireRestart = false, HintText = RefValues.GenericHint)]
         public float NumClassesAdjustment { get; set; } = 1f;
 
-        [SettingPropertyGroup("Misc")]
-        [SettingPropertyBool("Weapon class helper", Order = 1, RequireRestart = false, HintText = "Enable this to display the weapon class the player is currently using on attack, mainly for debug purposes.")]
+        [SettingPropertyGroup(RefValues.MiscText)]
+        [SettingPropertyBool(RefValues.WepHelperText, Order = 1, RequireRestart = false, HintText = RefValues.WepHelperHint)]
         public bool WepClassHelper { get; set; } = false;
 
         public override string Id { get { return base.GetType().Assembly.GetName().Name; } }
